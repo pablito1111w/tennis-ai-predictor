df82244a6df43a83b25063a2b6ea09057e178b9c
pablito1111w <vgsks@yahoo.com>
2026-07-23 13:51:50 +0300

    Update tennis_data.py

--- tennis_data.py
+++ tennis_data.py
@@ -8,13 +8,14 @@ ALLOWED_TOURNAMENTS = [
 ]
 
 
+
 def get_atp_matches():
 
     today = datetime.now().strftime("%Y-%m-%d")
 
 
-    # Laikina struktūra.
-    # Vėliau šią vietą pakeis realus duomenų gavimas.
+    # TESTINIAI DUOMENYS
+    # Vėliau čia jungsim realų šaltinį
 
     all_matches = [
 
@@ -25,64 +26,95 @@ def get_atp_matches():
 
             "surface": "Hard",
 
+
             "player1": {
+
                 "name": "Jannik Sinner",
+
                 "ranking": 1,
-                "form": "8 wins / 10 matches",
+
+                "form": "8/10",
+
                 "serve_rating": 9,
+
                 "return_rating": 9
             },
 
 
             "player2": {
+
                 "name": "Daniil Medvedev",
+
                 "ranking": 6,
-                "form": "6 wins / 10 matches",
+
+                "form": "6/10",
+
                 "serve_rating": 8,
+
                 "return_rating": 8
             },
 
 
             "h2h": "Sinner leads 6-5",
 
+
             "odds": {
+
                 "player1": 1.55,
+
                 "player2": 2.60
             }
+
         },
 
 
         {
+
             "date": today,
 
             "tournament": "ATP 500",
 
             "surface": "Clay",
 
+
             "player1": {
+
                 "name": "Carlos Alcaraz",
+
                 "ranking": 2,
-                "form": "9 wins / 10 matches",
+
+                "form": "9/10",
+
                 "serve_rating": 9,
+
                 "return_rating": 9
             },
 
 
             "player2": {
+
                 "name": "Casper Ruud",
+
                 "ranking": 8,
-                "form": "7 wins / 10 matches",
+
+                "form": "7/10",
+
                 "serve_rating": 8,
+
                 "return_rating": 8
             },
 
 
             "h2h": "Alcaraz leads 4-1",
 
+
             "odds": {
+
                 "player1": 1.40,
+
                 "player2": 3.00
             }
+
         }
 
     ]
@@ -103,14 +135,20 @@ def get_atp_matches():
     if len(filtered) == 0:
 
         return {
+
             "status": "empty",
+
             "message":
             "🎾 Šiandien nėra tinkamų ATP TOP lygio mačų"
+
         }
 
 
 
     return {
+
         "status": "ok",
+
         "matches": filtered
+
     }
